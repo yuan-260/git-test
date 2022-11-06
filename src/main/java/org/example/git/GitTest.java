5fef017dd6b01a0014b279b748a350c708580390
yuan-260 <llmmyy260@163.com>
2022-11-06 19:31:37 +0800

    初次提交

---
+++ src/main/java/org/example/git/GitTest.java
@@ -0,0 +1,15 @@
+package org.example.git;
+
+/**
+ * @BelongsProject: git-test
+ * @BelongsPackage: org.example.git
+ * @Author: lmy
+ * @CreateTime: 2022/11/6 19:27
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("helle git");
+
+    }
+}
